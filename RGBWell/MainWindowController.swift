a03cc9839b46e8fad6b2e6e628561870410d5898
Michael Zornek <mike@clickablebliss.com>
2015-05-20 17:04:36 -0400

    Refactored to be bindings-based.

--- RGBWell/MainWindowController.swift
+++ RGBWell/MainWindowController.swift
@@ -2,49 +2,25 @@ import Cocoa
 
 class MainWindowController: NSWindowController {
     
-    @IBOutlet weak var redSlider: NSSlider!
-    @IBOutlet weak var greenSlider: NSSlider!
-    @IBOutlet weak var blueSlider: NSSlider!
+    //MARK: - Properties
     
-    @IBOutlet weak var colorWell: NSColorWell!
-    
-    var red = 0.0
-    var green = 0.0
-    var blue = 0.0
-    var alpha = 1.0
-    
-    override func windowDidLoad() {
-        super.windowDidLoad()
-        redSlider.doubleValue = red
-        greenSlider.doubleValue = green
-        blueSlider.doubleValue = blue
-        updateColor()
+    dynamic var currentColor: NSColor {
+        return NSColor(calibratedRed: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: CGFloat(alpha))
     }
+    dynamic var red = 0.0
+    dynamic var green = 0.0
+    dynamic var blue = 0.0
+    dynamic var alpha = 1.0
+    
+    //MARK: - NSWindowController
     
     override var windowNibName: String? {
         return "MainWindowController"
     }
     
-    @IBAction func adjustRed(sender: NSSlider) {
-        println("R slider's value \(sender.doubleValue)")
-        red = sender.doubleValue
-        updateColor()
-    }
+    //MARK: - NSKeyValueObserving
     
-    @IBAction func adjustGreen(sender: NSSlider) {
-        println("G slider's value \(sender.doubleValue)")
-        green = sender.doubleValue
-        updateColor()
-    }
-    
-    @IBAction func adjustBlue(sender: NSSlider) {
-        println("B slider's value \(sender.doubleValue)")
-        blue = sender.doubleValue
-        updateColor()
-    }
-    
-    func updateColor() {
-        let newColor = NSColor(calibratedRed: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: CGFloat(alpha))
-        colorWell.color = newColor
+    class func keyPathsForValuesAffectingCurrentColor() -> NSSet {
+        return Set(["red", "green", "blue"])
     }
 }
